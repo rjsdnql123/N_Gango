b72e862d4a6c094662f3b8edcbedabf4947cbdd6
Kjoobin <imkj1148@gmail.com>
2020-05-19 17:02:43 +0900

    Init Njango use ts

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import sequelize from './model';
+const { Users } = sequelize;
+
+const PORT = process.env.PORT || '3001';
+
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log('/');
+  Users.create({
+    email: 'test',
+    password: '1234',
+    username: 'nameOfUser',
+    token: 'A73628ANDJS'
+  });
+  res.send('Success');
+});
+
+app.listen(PORT, () => {
+  console.log('App started port : ', PORT);
+});
