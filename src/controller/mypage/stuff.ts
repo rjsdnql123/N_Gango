b32157f7ee95605f739fceeac5e884247f98dc84
Kjoobin <imkj1148@gmail.com>
2020-05-21 22:06:42 +0900

    Add mypage regist stuff when after login

---
+++ src/controller/mypage/stuff.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import sequelize from '../../models';
+const { Users, Stuffs, UserStuff } = sequelize;
+
+const addStuff = async function(req: Request, res: Response) {
+  try {
+    const { data, id } = req.body;
+    const user = await Users.findOne({ where: { id: data } }).then(
+      (res): any => res
+    );
+    if (!user) {
+      return res
+        .status(403)
+        .send({ error: { message: 'Bad request not user' } });
+    }
+    const stuff = await Stuffs.findOne({ where: { id } }).then(
+      (res): any => res
+    );
+    if (!stuff) {
+      return res
+        .status(403)
+        .send({ error: { message: 'Bad request not stuff' } });
+    }
+    const [result, created] = await UserStuff.findOrCreate({
+      where: { userId: user.id, stuffId: stuff.id },
+    });
+    if (!created) {
+      await UserStuff.destroy({ where: { id: result.get('id') } });
+      await res.status(201).send({ response: false });
+    }
+    return res.status(201).send({ response: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('server error');
+  }
+};
+
+module.exports = addStuff;
