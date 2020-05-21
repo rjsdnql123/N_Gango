5e014a4a126d6e3b49edd615cd60e65d2849b999
Kjoobin <imkj1148@gmail.com>
2020-05-21 13:19:28 +0900

    Modify middleware -> helper function

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -2,27 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 require('dotenv').config();
 
-export default async function(req: Request, res: Response, next: NextFunction) {
-  try {
-    const { token } = req.body;
-    if (!token) {
-      return res.status(404).send({ error: { message: 'Login require' } });
-    }
-    const decoded = await jwt.verify(token, process.env.TOKEN);
-    console.log(decoded);
-    // req.verify = decoded.valueOf();
-    next();
-  } catch (err) {
-    return res.status(500).send({ error: { message: 'server error' } });
-  }
+export async function jwtVerify(token: string): Promise<any> {
+  const decoded: any = await jwt.verify(token, process.env.TOKEN);
+  console.log(decoded);
+  return decoded;
 }
 
 export function jwtSign(verify: any): string {
   return jwt.sign(
     {
-      exp: 60 * 60,
       data: verify,
     },
-    process.env.TOKEN
+    process.env.TOKEN,
+    { expiresIn: '1h' }
   );
 }
