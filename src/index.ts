ea04211f067e531d6758d9814fa38d1c314e2218
Kjoobin <imkj1148@gmail.com>
2020-05-21 13:19:04 +0900

    Add mypage routing

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ const cors = require('cors');
 // const router = express.Router()
 const { Users } = sequelize;
 const userRouter = require('./routes/user');
+const mypageRouter = require('./routes/mypage');
 
 const PORT = process.env.PORT || '3001';
-
-
 
 // const signin = require('./controller/signin')
 
 // const signup = require('./controller/signup')
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/user', userRouter)
+app.use('/user', userRouter);
+app.use('/mypage', mypageRouter);
 
 // app.get('/signin', signin.get)
 // app.post('/signup', signup.post)
