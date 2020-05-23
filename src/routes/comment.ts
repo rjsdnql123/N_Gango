4ddd29c17b36eb55510edd5399c05506457fce28
Kjoobin <imkj1148@gmail.com>
2020-05-23 16:26:52 +0900

    Add specify user comment

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 const { commentController } = require('../controller');
 
 router.post('/', commentController.comment);
+router.get('/comment', commentController.comment);
 
 module.exports = router;
