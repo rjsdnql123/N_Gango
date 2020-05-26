a60729af5b3482c9eb5ba1594da309a1bd4163ff
won <rjsdnql123@gmail.com>
2020-05-26 15:08:11 +0900

    category

--- src/controller/stuff/categorySearch.ts
+++ src/controller/stuff/categorySearch.ts
@@ -2,27 +2,24 @@ import express, { Request, Response, NextFunction } from 'express';
 import sequelize from '../../models';
 const opSequelize = require("sequelize");
 const Op = opSequelize.Op
-const { Category, Stuffs } = sequelize;
+const { Category, Stuffs, StuffRecipe } = sequelize;
 
 const categorySearch = async function(req: Request, res: Response) {
     try{
-      let search = req.body.categoryName
+      let categoryName = req.body.categoryName
       await Category.findAll({
           where: {
               name: {
-                [Op.like]: "%"+search+"%"
+                [Op.like]: "%"+categoryName+"%"
               }
           },
-          include: [{model: Stuffs}]
-      }).then(result => {
-          if(result.length){
-          res.status(200).send(result)
+      }).then((result) => {
+          if(result.length) {
+              res.status(200).send(result)
           } else {
-              res.status(404).send({ error: { message: '카테고리와 일치하는거 없음' } })
+              res.status(404).send({error: '카테고리 없음'})
           }
       })
-
-
     }catch(error) {
         console.log(error)
         res.status(500).send(error)
