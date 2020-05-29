91158a253d005336b05dba48f3d2b1a9ff8150d2
Kjoobin <imkj1148@gmail.com>
2020-05-29 12:33:37 +0900

    Modify recipe query

--- src/controller/recipe/getRecipe.ts
+++ src/controller/recipe/getRecipe.ts
@@ -1,25 +1,24 @@
-import express, { Request, Response, NextFunction } from "express";
-import sequelize from "../../models";
+import express, { Request, Response, NextFunction } from 'express';
+import sequelize from '../../models';
 const { Recipes, Stuffs } = sequelize;
 
 const getRecipe = async function(req: Request, res: Response) {
-  let params = req.params.id.slice(1)
-  try{
-    const recipe = await Recipes.findAll({ where: { id: params },
-    include: [{model: Stuffs}]
-    }).then(
-      (res): any => res
-    )
+  let params = req.query.id;
+  console.log('PARAMS', params);
+  try {
+    const recipe = await Recipes.findAll({
+      where: { id: params },
+      include: [{ model: Stuffs }],
+    }).then((res): any => res);
     if (!recipe) {
-      res.status(404).send('레시피가 없음')
+      res.status(404).send('레시피가 없음');
     } else {
-      res.status(200).send(recipe)
+      res.status(200).send(recipe);
     }
-    
-  } catch(error){
-    console.log(error)
-    res.status(500).send('server error')
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('server error');
   }
-  }
+};
 
 module.exports = getRecipe;
