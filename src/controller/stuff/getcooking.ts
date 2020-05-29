30ca060f34c3e8552e11d0fbff95ad3fdee5b4f2
Kjoobin <imkj1148@gmail.com>
2020-05-29 10:54:04 +0900

    Add include sttfs when get cookking

--- src/controller/stuff/getcooking.ts
+++ src/controller/stuff/getcooking.ts
@@ -2,28 +2,27 @@ import { Request, Response } from 'express';
 import sequelize from '../../models';
 const { Recipes, Stuffs } = sequelize;
 
-const getCooking = async function(req:any, res:Response) {
-    try{
-      const stuffs =req.query.stuffname
-      const arr:any = [];
-      for(let i = 0; i <stuffs.length; i++){
-      let { stuffname } = JSON.parse(req.query.stuffname[i])
-      console.log(stuffname)
+const getCooking = async function(req: any, res: Response) {
+  try {
+    const stuffs = req.query.stuffname;
+    const arr: any = [];
+    for (let i = 0; i < stuffs.length; i++) {
+      let { stuffname } = JSON.parse(req.query.stuffname[i]);
+      console.log(stuffname);
       await Stuffs.findAll({
-           where: { stuffname: stuffname },
-           include:  [{model: Recipes}]
-      }).then((res:any) => arr.push(res))
-      }
-      if(arr[0].length) {
-        res.status(200).send(arr[0])
-      } else{
-        res.status(404).send('not stuff')
-      }
-
-    } catch(error) {
-      console.log(error)
-      res.status(500).send('server error')
+        where: { stuffname: stuffname },
+        include: [{ model: Recipes, include: [Stuffs] }],
+      }).then((res: any) => arr.push(res));
     }
-}
+    if (arr[0].length) {
+      res.status(200).send(arr[0]);
+    } else {
+      res.status(404).send('not stuff');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('server error');
+  }
+};
 
-module.exports = getCooking
+module.exports = getCooking;
